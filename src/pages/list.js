adaec1b7accb8b83c9cd4bcb7b64ad01c621a0d0
chiakihonda <chiaki.honnda@gmail.com>
2020-05-12 09:29:48 +0900

    map

--- src/pages/list.js
+++ src/pages/list.js
@@ -1,59 +1,63 @@
 import React from 'react';
 
+const todoList= [{
+    id:'1',
+    title:'タイトル1',
+    content:'内容1内容1内容1内容1',
+    createAt:'2020-5-04',
+    category:'1',
+},
+{
+    id:'2',
+    title:'タイトル2',
+    content:'内容2内容2内容2内容2',
+    createAt:'2020-5-04',
+    category:'2',
+},
+{
+    id:'3',
+    title:'タイトル3',
+    content:'内容3内容3内容3内容3',
+    createAt:'2020-5-04',
+    category:'1',
+}
+];
+
 class List extends React.Component {
     render(){
         return(
             <div className="wrapper">
-      <header>
-         <button>List(active)</button><button>Set</button>
-         <h1>Todo List</h1>
-      </header>
-      <body>
-        <table className="todo">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>タイトル</th>
-              <th>内容</th>
-              <th>日時</th>
-              <th>カテゴリ</th>
-              <th>操作</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>タイトル1</td>
-              <td>内容1内容1</td>
-              <td>2020-5-4</td>
-              <td>1</td>
-              <td><button>お気に入り</button>
-                  <button>お気に入り</button>
-                  <button>削除</button></td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>タイトル2</td>
-              <td>内容2内容2</td>
-              <td>2020-5-4</td>
-              <td>2</td>
-              <td><button>お気に入り</button>
-                <button>お気に入り</button>
-                <button>削除</button></td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>タイトル3</td>
-              <td>内容3内容3</td>
-              <td>2020-5-4</td>
-              <td>1</td>
-              <td><button>お気に入り</button>
-                <button>お気に入り</button>
-                <button>削除</button></td>
-            </tr>
-	        </tbody>
-        </table>
-      </body>
+            <header>
+              <button>List(active)</button><button>Set</button>
+              <h1>Todo List</h1>
+            </header>
+                    
+              <table className="todo">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>タイトル</th>
+                    <th>内容</th>
+                    <th>日時</th>
+                    <th>カテゴリ</th>
+                    <th>操作</th>
+                  </tr>
+                </thead>
+                <tbody>
+                {todoList.map((ii)=>(
+                  <tr>
+                        <td>{ii.createAt}</td>
+                        <td>{ii.title}</td>
+                        <td>{ii.content}</td>
+                        <td>{ii.createAt}</td>
+                        <td>{ii.category}</td>
+                        <td><button>お気に入り</button>
+                        <button>お気に入り</button>
+                        <button>削除</button></td>
+                  </tr>
+                ))}
+                </tbody>
+              </table>
     </div>
         );
     }
