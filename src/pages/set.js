54fdffa2165091e608ca74a3f9d9b8812a2930ec
chiakihonda <chiaki.honnda@gmail.com>
2020-05-12 17:51:38 +0900

    set.jsの作成

--- src/pages/set.js
+++ src/pages/set.js
@@ -1,1 +1,20 @@
 import React from 'react';
+
+class Set extends React.Component {
+    render(){
+        return(
+            <div className="setPage">
+                <h1>登録画面</h1>
+                <p>タイトル</p>
+                <textarea></textarea>
+                <p>内容</p>
+                <textarea></textarea>
+                <p>カテゴリ</p>
+                <textarea></textarea>
+            </div>
+        );
+    }
+  }
+
+  export default Set;
+  
